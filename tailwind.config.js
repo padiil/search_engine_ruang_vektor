c1c55ba6b612a237558758a1c44811672192fad4
padiil <fadhilgani2@gmail.com>
2024-11-28 21:03:06 +0700

    fix tailwind issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ module.exports = {
   content: [
     "./templates/**/*.{html,js}", // Hanya untuk folder templates
     "./static/**/*.{html,js}", // Atau tambahkan jalur spesifik lainnya
+    "./app/**/*.{html,js}", // Atau tambahkan jalur spesifik lainnya
   ],
   theme: {
     extend: {},
